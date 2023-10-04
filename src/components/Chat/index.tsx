929b566a7cd5e7e96bc32401f514693e4cc97329
kemosaaabe <sanchellos27@mail.ru>
2023-10-04 19:10:38 +0300

    socket

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,4 +1,7 @@
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import checkToken from '../../api/checkToken';
 
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
@@ -8,6 +11,48 @@ import Button from '@mui/material/Button';
 
 const Chat = () => {
   const { chatId } = useParams();
+
+  const socket = new WebSocket(`ws://localhost:8000/ws/chat/chat${chatId}/`);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const getChat = async () => {
+      const isTokenValid = await checkToken();
+
+      if (isTokenValid) {
+        socket.onopen = () => {
+          console.log('Соединение установлено');
+        };
+
+        socket.onclose = () => {
+          console.log('Соединение закрыто');
+        };
+
+        socket.onmessage = (data: MessageEvent<string>) => {
+          console.log(data);
+        };
+
+        socket.onerror = (error) => {
+          console.log(error);
+        };
+      } else {
+        navigate('/auth');
+      }
+    };
+
+    getChat();
+  }, []);
+
+  const [messages, setMessages] = React.useState<string[]>([]);
+  const [value, setValue] = React.useState('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const onMessageEnter = () => {
+    setValue('');
+  };
 
   return (
     <Container maxWidth="md">
@@ -19,8 +64,19 @@ const Chat = () => {
           color: 'white',
         }}
       >
-        <TextField placeholder="Введите сообщение" sx={{ width: '100%' }} />
-        <Button type="submit" fullWidth variant="contained" sx={{ mb: 2 }}>
+        <TextField
+          value={value}
+          onChange={onChange}
+          placeholder="Введите сообщение"
+          sx={{ width: '100%' }}
+        />
+        <Button
+          onClick={onMessageEnter}
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mb: 2 }}
+        >
           Отправить сообщение
         </Button>
       </Box>
